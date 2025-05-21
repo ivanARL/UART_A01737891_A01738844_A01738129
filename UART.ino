8ab202cc1d31b362eb0d5f72acd5d93ff98f7afd
ivanARL <143296062+ivanARL@users.noreply.github.com>
2025-05-20 22:50:57 -0600

    Create UART.ino

---
+++ UART.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hola");
+  delay(1000);
+  Serial.println("nosotros");
+  delay(1000);
+  Serial.println("somos");
+  delay(1000);
+  Serial.println("Bruno");
+  delay(1000);
+  Serial.println("Omar");
+  delay(1000);
+  Serial.println("Ivan");
+  delay(1000);
+  Serial.println("Gracias");
+  delay(3000);  // Espera más tiempo antes de repetir
+}
